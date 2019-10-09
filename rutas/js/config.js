46cc5aa868fea33be80a5101654b2e1d8f1a7fe0
Aritz Campo <acampoz@gmail.com>
2019-10-09 13:02:37 +0200

    rutas acabado

---
+++ rutas/js/config.js
@@ -0,0 +1,25 @@
+// var app = angular.module( "app", [ ] );
+app.config( function( $routeProvider ){
+
+  $routeProvider
+    .when('/',{
+        templateUrl: 'parciales/home.html'
+    })
+    .when('/alumnos',{
+        templateUrl: 'parciales/alumnos.html'
+    })
+    .when('/profesores',{
+        templateUrl: 'parciales/profesores.html'
+    })
+    .when('/creditos',{
+      templateUrl: 'parciales/creditos.html'
+  })
+  .when('/componente1',{
+    templateUrl: 'parciales/componente.html'
+})
+  
+    .otherwise({
+      redirectTo: '/'
+    })
+
+})
