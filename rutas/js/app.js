782dad6bae2694568e8a95e9ea6a34c1e5bb291b
Aritz Campo <acampoz@gmail.com>
2019-10-10 10:34:58 +0200

    a

--- rutas/js/app.js
+++ rutas/js/app.js
@@ -4,6 +4,18 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
   $scope.menuSuperior = "parciales/menu.html";
 
- 
+ this.$onInit = function(){
+   console.log('onInit mainCtrl');
+//check para saber si el servicio esta levantado
+let url='http://localhost:3000/frutas'
+   $http.get(url).then(function (result) {
+     console.trace('servicio rest funcionando %o',result);
+     
+   }).catch(function (response) {
+     console.warn('servicio rest fallando %o', response);
+     
+   });
+   
+ }//onInit
 
 }]);
